bd715badb2985a5d3daa2b11cb4dbfcb0f6c8b4e
Guipengtt <xgp980303@gmail.com>
2022-04-19 17:25:29 +0800

    update

---
+++ src/main/java/server/MyServer.java
@@ -0,0 +1,39 @@
+package server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    public static void main(String[] args) throws Exception{
+        // 创建两个线程组 bossGroup, workerGroup
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            // 创建服务端的启动对象，设置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            // 设置两个线程组bossGroup和 workerGroup
+            bootstrap.group(bossGroup, workerGroup)
+                    // 设置服务器端通道实现类型
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new MyServerHandler());
+                        }
+                    });
+            System.out.println("Java技术爱好者的服务器端已经准备就绪...");
+            ChannelFuture channelFuture = bootstrap.bind(6666).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
