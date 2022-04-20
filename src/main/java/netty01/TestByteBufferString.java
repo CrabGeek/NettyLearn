eee17b74adcd14e5c027ec7b37d78221e3a5467e
Guipengtt <xgp980303@gmail.com>
2022-04-20 14:41:29 +0800

    update

--- src/main/java/netty01/TestByteBufferString.java
+++ src/main/java/netty01/TestByteBufferString.java
@@ -1,4 +1,34 @@
 package netty01;
 
+import util.ByteBufferUtil;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
+
 public class TestByteBufferString {
+    public static void main(String[] args) {
+        //1. 字符串转为ByteBuffer
+        String hello = "hello";
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+        buffer.put(hello.getBytes());
+        ByteBufferUtil.debugAll(buffer);
+
+        //2. Charset
+        ByteBuffer encode = StandardCharsets.UTF_8.encode(hello);
+        ByteBufferUtil.debugAll(encode);
+
+        // 3. wrap
+        ByteBuffer wrap = ByteBuffer.wrap(hello.getBytes());
+        ByteBufferUtil.debugAll(wrap);
+
+
+        // ByteBuffer转字符串
+        CharBuffer decode = StandardCharsets.UTF_8.decode(encode);
+        System.out.println(decode.toString());
+
+        buffer.flip();
+        CharBuffer decode2 = StandardCharsets.UTF_8.decode(buffer);
+        System.out.println(decode2.toString());
+    }
 }
